032a4ec548cca8c597b4263cc70fab15d614951b
jisun <jisunnala@gmail.com>
2024-12-16 22:54:05 +0900

    get img from back

--- src/components/Place.js
+++ src/components/Place.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 function Place({ place }) {
     return (
-        <div> 
-            <Card style={{ width: '18rem', margin: '10px' }}>
-               {/* <Card.Img variant="top" src={place.image} />  이미지 데이터 주석 처리 */}
-                <Card.Body>
-                    <Card.Title>{place.placeName}</Card.Title> {/* 장소 이름 */}
-                
-                    <Card.Text>
-                        <strong>{place.region}</strong> 
-                    </Card.Text> 
-                    <Button variant="primary">관련사이트 이동</Button>
-                </Card.Body>
-            </Card>
+        <Button 
+        style={{ display: 'flex', alignItems: 'center', width: '31rem', height: '5rem', margin: '10px', border: '1px solid #ddd', borderRadius: '5px', overflow: 'hidden', textAlign: 'left', padding: '0' }} 
+        variant="light" // 버튼 색상 설정
+        onClick={() => console.log("Button clicked")} // 버튼 클릭 이벤트 예시
+    >
+        <img 
+            style={{ width: '8rem', height: '5rem', objectFit: 'cover' }} // 이미지 크기 조정
+            src={place.imagePath} 
+            alt={place.placeName}
+        />
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', padding: '0.5rem', height: '100%' }}>
+            <h5 style={{ margin: '0', fontSize: '1rem', paddingTop: '0.2rem' }}>{place.placeName}</h5> {/* 장소 이름 */}
+            <div style={{ fontSize: '0.9rem', marginTop: 'auto' }}>             
+            </div>
         </div>
+    </Button>
     );
 }
 
